8b71a62ce57e041aee251dffbcf8ff6d54c1b826
Jacob Riesser <riesserj@msu.edu>
2013-02-20 15:36:31 -0500

    ran into some git trouble, initial commit again

---
+++ drinkz/recipes.py
@@ -0,0 +1,8 @@
+class Recipe(object):
+	def __init__(self,name,ingredients):
+		self.Name = name.lower()
+		self.Ingredients = ingredients
+		Recipe.recipeCnt += 1
+
+	def need_ingridients():
+		pass
